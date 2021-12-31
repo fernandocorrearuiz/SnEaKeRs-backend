193a8b5693fc53d334b44e419d824779ab6eb0d4
fernandocorrearuiz <fernandocorrearuiz@gmail.com>
2021-12-31 20:45:01 +0100

    creating dummy data w Postman + Get Prodcts requst

--- src/index.js
+++ src/index.js
@@ -20,6 +20,16 @@ app.post("/products", (req, res) => {
     });
 });
 
+app.get("/products", (req, res) => {
+  Product.find({})
+    .then((products) => {
+      res.send(products);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
