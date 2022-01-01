f1f5959bedf9158a972db7ed0935956cbaa18a1e
fernandocorrearuiz <fernandocorrearuiz@gmail.com>
2022-01-01 13:47:05 +0100

    refactor async/await + Express router

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
 const express = require("express");
 require("./db/mongoose");
-const Product = require("./models/product");
+const productRouter = require("./routers/product");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/products", (req, res) => {
-  const product = new Product(req.body);
-
-  product
-    .save()
-    .then(() => {
-      res.send(product);
-    })
-    .catch((e) => {
-      res.status(400).send(e);
-    });
-});
-
-app.get("/products", (req, res) => {
-  Product.find({})
-    .then((products) => {
-      res.send(products);
-    })
-    .catch((e) => {
-      res.status(500).send(e);
-    });
-});
+app.use(productRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
---
+++ src/routers/product.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Product = require("../models/product");
+const router = new express.Router();
+
+router.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+
+  try {
+    await product.save();
+    res.status(201).send(product);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.send(products);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
