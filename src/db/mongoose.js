37434966213836179e5ca16d3483e8f2d14630db
fernandocorrearuiz <fernandocorrearuiz@gmail.com>
2022-08-15 21:09:29 +0200

    command to connect to MongoDB database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,3 +4,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/SnEaKeRs-api", {
   useNewUrlParser: true,
   // useCreateIndex: true,
 });
+
+// Run the following command to connect to MongoDB database
+// /Users/fernandocorrearuiz/mongodb/bin/mongod --dbpath=/Users/fernandocorrearuiz/mongodb-data
